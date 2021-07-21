d561ac6e0753a0d73482f7012f17b9b8947023bf
loa123 <4455716+loa123@users.noreply.github.com>
2021-07-21 11:51:40 +0800

    更新天气预报详情

--- plugins/caiyun_weather.js
+++ plugins/caiyun_weather.js
@@ -48,9 +48,9 @@ class Plugin extends Bot {
 > <font color="warning">${data.result.minutely.description.trim()}</font>
 
 **天气预报：**
-> <font color="info">现在温度： ${data.result.realtime.temperature.trim()}</font>
-> <font color="info">PM2.5：   ${data.result.realtime.air_quality.pm25.trim()}</font>
-> <font color="info">空气质量： ${data.result.realtime.air_quality.description.chn.trim()}</font>
+> 现在温度： <font color="info">${data.result.realtime.temperature.trim()}</font>
+> PM2.5：   <font color="info">${data.result.realtime.air_quality.pm25.trim()}</font>
+> 空气质量：  <font color="info">${data.result.realtime.air_quality.description.chn.trim()}</font>
 > <font color="info">${data.result.hourly.description.trim()}</font>
 
 ${alert_md}`);
